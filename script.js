079a40aa5d787473743c4015aa1e56c2152e4777
Bitcoins4free <bitcoins4free@proton.me>
2024-11-28 18:28:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Initialize EmailJS
+(function () {
+    emailjs.init("kL8LldtVITo1oHfSk"); // Your public EmailJS API key
+})();
+
 // Form submission handling
 document.getElementById("giveawayForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the form from reloading the page
@@ -16,7 +21,7 @@ document.getElementById("giveawayForm").addEventListener("submit", function (eve
     emailjs.send("service_4wb3isl", "template_e8vem1j", formData)
         .then(
             function(response) {
-                console.log("Email sent successfully:", response);
+                console.log("Success:", response);
                 alert("Thank you! Your submission has been sent.");
                 document.getElementById("giveawayForm").reset(); // Clear the form after submission
             },
